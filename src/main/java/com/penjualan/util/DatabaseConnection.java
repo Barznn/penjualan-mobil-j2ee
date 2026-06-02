b828da98069396380f83c927dc467b67ccb894d5
Barznn <rizkiarkaan@gmail.com>
2026-06-02 22:12:37 +0700

    Add database connection utility

---
+++ src/main/java/com/penjualan/util/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package com.penjualan.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_penjualan_mobil";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Gagal koneksi ke database: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
